cc021b3a69578e3fa21149d2c6d489566d14655f
Murad Ali <murad.manni@gmail.com>
2022-03-30 21:55:19 +1100

    declared variables in javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// --------------------   VARIABLES DECLARED  -----------------------
+var btnSearch = document.querySelector("#btn-search");
+var sectionSearch = document.querySelector("#section-search");
+var textboxSearch = document.querySelector("#textbox-search");
+
+
+var omdbApiKey="ef78856e";
+var omdbUrl ="https://www.omdbapi.com/?apikey=" + omdbApiKey + "&type=movie&s=";
+var omdbSingleSearchUrl = "https://www.omdbapi.com/?apikey=" + omdbApiKey + "&i="
+
+var pageNumber;
+var totalPages;
+var totalMovies;
